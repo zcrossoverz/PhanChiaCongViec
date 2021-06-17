f7d88b39fefc38df20589b1d73e674e7175b2383
zcrossoverz <56151228+zcrossoverz@users.noreply.github.com>
2021-06-17 11:50:13 +0700

    fix interface css, add handle result

--- assets/script.js
+++ assets/script.js
@@ -9,17 +9,27 @@ const content_popup = document.querySelector('#content_popup');
 const method = document.querySelector('#method');
 const inp = document.querySelector('#inp');
 const act = document.querySelector('#act');
+const start = document.querySelector('#start');
+const rs = document.querySelector('.results');
+let jobs = [], users = [];
+let result = [];
 function c(e){
     let textnode = document.createTextNode(e);
     let temp = document.createElement('p');
-    temp.className = 'jobItem';
+    temp.className = 'Item';
     temp.appendChild(textnode);
-    if(document.querySelector('#method').innerText==1) job.appendChild(temp);
-    else user.appendChild(temp);
+    if(document.querySelector('#method').innerText==1){
+        job.appendChild(temp);
+        jobs.push(e);
+    }else{
+        user.appendChild(temp);
+        users.push(e);
+    } 
 }
 
 document.querySelector('#submit_popup').addEventListener('click',()=>{
     c(inp.value);
+    inp.value = '';
     popup.style = 'display:none';
     main.style = 'display:block';
 });
@@ -31,22 +41,51 @@ let cancer = ()=>{
         popup.style = 'display:none';
         main.style = 'display:block';
     });
-
+    inp.value = '';
 }
 
 btnJob.addEventListener('click',()=>{
     method.innerText = 1;
-    title_popup.innerText = 'Them cong viec';
-    content_popup.innerText = 'Nhap cong viec';
+    title_popup.innerText = 'Thêm công việc';
+    content_popup.innerText = 'Nhập công việc';
     popup.style = 'display:block';
     main.style = 'display:none';
     cancer();
 });
 btnUs.addEventListener('click',()=>{
     method.innerText = 2;
-    title_popup.innerText = 'Them nguoi tham gia';
-    content_popup.innerText = 'Nhap ten';
+    title_popup.innerText = 'Thêm người tham gia';
+    content_popup.innerText = 'Nhập tên';
     popup.style = 'display:block';
     main.style = 'display:none';
     cancer();
 });
+start.addEventListener('click',()=>{
+    if(users.length >= jobs.length){
+        while(jobs.length>0){
+            let x = Math.floor(Math.random()*users.length);
+            result[jobs.pop()] = users[x];
+            users.splice(x,1);
+        }
+    }else{
+        while(users.length>0){
+            let x = Math.floor(Math.random()*jobs.length);
+            result[users.pop()] = jobs[x];
+            jobs.splice(x,1);
+        }
+    }
+    // result.forEach((e,i)=>{
+    //     let ts = document.createTextNode(e+' - '+i);
+    //     let dc = document.createElement('p');
+    //     dc.className = 'Item';
+    //     dc.appendChild(ts);
+    //     rs.appendChild(dc);
+    //     console.log(e+i);
+    // });
+    result.forEach((e,i)=>{
+        console(e+i);
+    });
+    document.querySelector('#result').classList.toggle('active');
+    document.querySelector('#container').classList.toggle('notactive');
+});
+
